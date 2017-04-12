9c3217a9bc5bacc6773f5919ef0eb75ccfcf789b
companyAccount <jeonil@sspon.com>
2017-04-12 13:54:45 +0900

    msg

--- js/js.js
+++ js/js.js
@@ -1,38 +1,44 @@
 var app = angular.module('angular-application', [])
     .controller('output-controller', function ($scope, imgur) {
-        $scope.thumbnails = imgur.getImages(function () {
-            $scope.$apply(function () {
-                $scope.thumbnails = imgur.images;
-            });
+        $scope.thumbnails = imgur.getImages();
+        $scope.$on('reloadImage', function () {
+            $scope.$apply();
         });
     })
-    .factory('imgur', function ($http) {
+    .factory('imgur', function ($http, $rootScope) {
         return {
             getImages: function () {
                 this.reload = arguments[0];
 
                 var arr = this.images;
-                for (var i = 0; i < 100; i++) {
+                for (var i = 0; i < 102; i++) {
                     arr.push(this.getImage());
                 }
                 return arr;
             },
             getImage: function () {
+
                 var img = new Image();
                 var arr = this.images;
                 var th = this;
+
                 img.addEventListener('load', function () {
+                    var index = arr.indexOf(this);
+
                     if (this.width === 161) {
-                        var index = arr.indexOf(this);
                         if (index !== -1) {
                             arr[index] = th.getImage();
-                            arr[index].addEventListener('load',function(){
-                                th.reload();
-                            });
                         }
+                    } else {
+                        $rootScope.$broadcast('reloadImage');
                     }
-                    img.style.opacity = 1;
                 });
+
+                img.addEventListener('error', function () {
+                    var index = arr.indexOf(this);
+                    arr[index] = th.getImage();
+                });
+
                 img.src = this.getRandomUrl();
 
                 return img;
